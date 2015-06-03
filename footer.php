fdd1445cbf469c45373cd5b17eeae8c3766b1eb5
joshuaiz <joshuaiz@me.com>
2015-06-02 23:12:45 -0700

    first commit

---
+++ footer.php
@@ -0,0 +1,35 @@
+			<footer class="footer" role="contentinfo">
+
+				<div id="inner-footer" class="wrap clearfix">
+
+					<nav role="navigation">
+							<?php bones_footer_links(); ?>
+					</nav>
+					
+					<div class="tooltip fade bottom in" style="top: 0px; left: 0px; display: block;float:left;"><div class="tooltip-arrow"></div><div class="tooltip-inner">Follow High Bias Mastering on Twitter: <a class="twitter-link" href="http://twitter.com/hbmastering">@hbmastering</a></div></div>
+					
+					
+
+<div class="client-guide">
+					<a href="http://hbmstatic.com/downloads/HighBiasMastering_2013_Client_Guide.pdf"><img src="http://hbmstatic.com/images/2013_hbm_client_guide_cover@2x.png" width="96px" height="124px"></a>
+					<p>Download our <a href="http://hbmstatic.com/downloads/HighBiasMastering_2013_Client_Guide.pdf">2013 Client Guide</a> with submission tips, FAQ, pricing and more.</p>
+					</div>
+
+					<p class="source-org copyright small">&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>. <a href="/faq/">FAQ</a> | <a href="/terms/">Terms & Conditions</a> | <a href="/contact/">Contact Us</a></p>
+
+				</div> <!-- end #inner-footer -->
+
+			</footer> <!-- end footer -->
+
+		</div> <!-- end #container -->
+
+<script>
+</script>
+
+
+		<!-- all js scripts are loaded in library/bones.php -->
+		<?php wp_footer(); ?>
+
+	</body>
+
+</html> <!-- end page. what a ride! -->
